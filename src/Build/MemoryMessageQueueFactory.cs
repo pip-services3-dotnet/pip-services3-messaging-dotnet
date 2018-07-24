547a2b85e94dc5b87a52a23e45958fee5d755bbd
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2018-07-23 20:34:58 -0700

    Initial commit

---
+++ src/Build/MemoryMessageQueueFactory.cs
@@ -0,0 +1,19 @@
+﻿using PipServices.Components.Build;
+using PipServices.Commons.Refer;
+
+namespace PipServices.Messaging.Queues
+{
+    public class MemoryMessageQueueFactory : Factory
+    {
+        public static readonly Descriptor Descriptor = new Descriptor("pip-services-messaging", "factory", "message-queue", "memory", "1.0");
+        public static readonly Descriptor MemoryQueueDescriptor = new Descriptor("pip-services-messaging", "message-queue", "memory", "*", "*");
+
+        public MemoryMessageQueueFactory()
+        {
+            Register(MemoryQueueDescriptor, (locator) => {
+                Descriptor descritor = (Descriptor)locator;
+                return new MemoryMessageQueue(descritor.Name);
+            });
+        }
+    }
+}
